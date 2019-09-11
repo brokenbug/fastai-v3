7b5c2ce7d6d5cd9f60e6430adc6cb410c0eff453
Tommy Svensson <tommy@Tommys-MacBook-Pro-2.local>
2019-09-11 18:28:59 +0200

    apple cherry snow

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/vpfr9j86ooz3xkr/export.pkl?dl=0'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['apple', 'cherry', 'snow']
 path = Path(__file__).parent
 
 app = Starlette()
@@ -27,7 +27,7 @@ async def download_file(url, dest):
             data = await response.read()
             with open(dest, 'wb') as f:
                 f.write(data)
-
+∏
 
 async def setup_learner():
     await download_file(export_file_url, path / export_file_name)
